e14b3ff7eb146c69a45dcf232ccfd8cb8ad1a11d
Sonick Mumba <kingellie.mumba@gmail.com>
2024-04-06 16:23:17 +0200

    Implement the tracklist component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,52 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import TrackList from "./components/tracklist/TrackList";
 import "./App.css";
+
+const tracks = [
+  {
+    id: 1,
+    name: "Bohemian Rhapsody",
+    artist: "Queen",
+    album: "A Night at the Opera",
+  },
+  {
+    id: 2,
+    name: "Stairway to Heaven",
+    artist: "Led Zeppelin",
+    album: "Led Zeppelin IV",
+  },
+  {
+    id: 3,
+    name: "Hotel California",
+    artist: "Eagles",
+    album: "Hotel California",
+  },
+  {
+    id: 4,
+    name: "Hey Jude",
+    artist: "The Beatles",
+    album: "Single",
+  },
+  {
+    id: 5,
+    name: "Smells Like Teen Spirit",
+    artist: "Nirvana",
+    album: "Nevermind",
+  },
+  {
+    id: 6,
+    name: "Imagine",
+    artist: "John Lennon",
+    album: "Imagine",
+  },
+];
 
 function App() {
   return (
     <>
-      <div>sonick</div>
+      <TrackList tracks={tracks} />
     </>
   );
 }
--- src/components/tracklist/TrackList.jsx
+++ src/components/tracklist/TrackList.jsx
@@ -1,7 +1,20 @@
 // import React from 'react'
 
-const TrackList = () => {
-  return <div></div>;
+const TrackList = (props) => {
+  return (
+    <div>
+      {props.tracks.map((track) => (
+        <div className="card" key={track.id}>
+          <h3>{track.name}</h3>
+          <div className="artist-album">
+            <span>{track.artist}</span>
+            <span>{track.album}</span>
+          </div>
+          <button className="add-button">+</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TrackList;
