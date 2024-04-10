c00453237ae74a2d930ce5c1344e28bfc51ad1c2
Sonick Mumba <kingellie.mumba@gmail.com>
2024-04-10 11:07:53 +0200

    Add spotyify file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import TrackList from "./components/tracklist/TrackList";
 import SearchBar from "./components/searchBar/SearchBar";
 import Playlist from "./components/playlist/Playlist";
-import options from "./components/util/options";
+// import options from "./components/util/options";
 import "./App.css";
 import { useState, useEffect } from "react";
+import Spotify from "./components/util/spotify";
 
 function App() {
   const [tracks, setTracks] = useState([]);
   const [loading, setLoading] = useState(false);
   const [playlist, setPlaylist] = useState([]);
   const [error, setError] = useState(null);
+
+  const [accessToken, setAccessToken] = useState(null);
 
   const handleClick = (e, id) => {
     e.preventDefault();
@@ -22,35 +25,82 @@ function App() {
     }
   };
 
-  const handleSearch = async (query) => {
-    if (query === "") return;
-    setLoading(true);
-    setError(null);
+  // const handleSearch = async (query) => {
+  //   if (query === "") return;
+  //   setLoading(true);
+  //   setError(null);
 
-    try {
-      const resp = await fetch(
-        `https://spotify23.p.rapidapi.com/search/?q=${query}&type=multi&offset=0&limit=10&numberOfTopResults=2`,
-        options
-      );
+  //   try {
+  //     const resp = await fetch(
+  //       `https://spotify23.p.rapidapi.com/search/?q=${query}&type=multi&offset=0&limit=10&numberOfTopResults=2`,
+  //       options
+  //     );
 
-      if (!resp.ok) {
-        throw new Error("Network response was not ok");
+  //     if (!resp.ok) {
+  //       throw new Error("Network response was not ok");
+  //     }
+  //     const data = await resp.json();
+  //     const newTracks = data.tracks.items.map((item) => ({
+  //       id: item.data.id,
+  //       name: item.data.name,
+  //       artist: item.data.artists.items[0].profile.name,
+  //       album: item.data.albumOfTrack.name,
+  //     }));
+  //     setTracks(newTracks);
+  //   } catch (error) {
+  //     setError(error.message);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  // const handleSearch = async (query) => {
+  //   try {
+  //     setLoading(true);
+  //     const data = await Spotify.search(query);
+  //     setTracks(data);
+  //   } catch (error) {
+  //     setError(error.message);
+  //     setLoading(false);
+  //   }
+  // };
+
+  // Added
+  useEffect(() => {
+    const fetchAccessToken = async () => {
+      try {
+        const token = await Spotify.getAccessToken();
+        setAccessToken(token);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false); // Set loading to false after fetching access token
       }
-      const data = await resp.json();
-      const newTracks = data.tracks.items.map((item) => ({
-        id: item.data.id,
-        name: item.data.name,
-        artist: item.data.artists.items[0].profile.name,
-        album: item.data.albumOfTrack.name,
-      }));
-      setTracks(newTracks);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
+    };
+    fetchAccessToken();
+  }, []);
+  // end
+  console.log(accessToken);
+
+  const handleSearch = (query) => {
+    if (query !== "") {
+      setLoading(true);
+      setError(null);
+
+      // Use access token from state in the search method
+      Spotify.search(query, accessToken)
+        .then((data) => {
+          setTracks(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setError(error.message);
+          setLoading(false);
+        });
     }
   };
 
+  console.log(tracks);
   const handleRemoveTrack = (e, id) => {
     e.preventDefault();
     setPlaylist((prev) => prev.filter((track) => track.id !== id));
